228eea3adc0902abc6e02ad597587aec1a5c9753
mohamed elsayed elrokapy <mohamedrad2022@gmail.com>
2025-05-14 12:35:25 +0300

    Update appcontext.js

--- src/users/context/appcontext.js
+++ src/users/context/appcontext.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 const islogedincontext = createContext({
-  isLoggedIn: "",
-  setIsLoggedIn: () => {},
+  isloggedin: "",
+  setisloggedin: () => {},
   logeduserinfo: {},
   setlogeduserinfo: () => {},
   products: [],
